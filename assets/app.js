8793cb9b0f97ba881044fb7668ba440653e731a9
Sébastien MAILLET <sebastien.maillet@gmail.com>
2025-12-02 12:15:32 +0100

    first commit

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,15 @@
 import './stimulus_bootstrap.js';
+import './js/scripts.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
